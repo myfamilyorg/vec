886882a3aa2f61c85097ce012fc3e4b336690d6f
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-08 15:51:10 -0700

    update vec

--- rust/lib.rs
+++ rust/lib.rs
@@ -420,8 +420,15 @@ impl<T> Vec<T> {
         let other_len = other.len();
         self.resize_impl(other_len + len)?;
         self.elements = other_len + len;
-        self.as_mut().slice_copy(&other)
+        unsafe {
+            ffi::write(2, "pre\n".as_ptr(), 4);
+        }
+        self.as_mut().slice_copy(&other)?;
         //slice_copy(other, self.slice_mut_from(len), other.len())
+        unsafe {
+            ffi::write(2, "post\n".as_ptr(), 5);
+        }
+        Ok(())
     }
 
     pub fn iter_mut(&mut self) -> VecRefMutIterator<'_, T> {
